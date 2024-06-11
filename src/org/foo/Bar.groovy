e90ca82251e831810144ff99ca7abbe6064de660
Harsh Bhargava <93305037+Harsh5006@users.noreply.github.com>
2024-06-11 16:32:37 +0530

    Update Bar.groovy

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -1,7 +1,13 @@
 package org.foo
 
-class Bar {
-    static void execute() {
-        sh "echo Hello World"
+class Bar implements Serializable {
+    def script
+
+    Bar(script) {
+        this.script = script
+    }
+
+    void execute() {
+        script.echo "Hello World"
     }
 }
